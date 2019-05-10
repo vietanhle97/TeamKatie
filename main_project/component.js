60e5a221b1883beccd8b7bdb3358bdd62fb7d98d
Hieu Dang <dangminhhieu781998@gmail.com>
2019-05-11 03:18:54 +0900

    do many many things

--- main_project/component.js
+++ main_project/component.js
@@ -1,4 +1,20 @@
+var currentTutorialName = "Exploding Kitten Normal Edition";
+var currentSpan = {"section": null, "start": null, "end": null};
 
+function createSpan(name){
+  var selectedRange = window.getSelection().getRangeAt(0);
+  var startPos = selectedRange.startOffset;
+  var endPos = selectedRange.endOffset;
+  if(startPos >= endPos) {
+    closeNav("mySidebar");
+    return;
+  }
+  currentSpan.section = name;
+  currentSpan.start = startPos;
+  currentSpan.end = endPos;
+  openNav("mySidebar", 'comment_tab');
+  console.log(currentSpan)
+}
 
 
 function create_div_to_carousel(n, name, link){
@@ -52,6 +68,7 @@ function add_to_collapse(name, head, carousel,text){
   var id = '#' + name;
 
   var all_content = document.getElementById('col-9');
+  console.log(all_content)
 
   var collapse = document.createElement('div');
   collapse.id = 'collapse';
@@ -88,6 +105,9 @@ function add_to_collapse(name, head, carousel,text){
   var card_body = document.createElement('div')
   card_body.className = 'card-body';
   card_body.innerHTML = text;
+  card_body.setAttribute("section-name",name);
+  card_body.addEventListener("mouseup",function(){
+                                      createSpan(this.getAttribute("section-name"))});
 
 
   if(name=='Components'){
@@ -170,8 +190,7 @@ function display(){
     add_to_carousel(exploding[i]['card'].toUpperCase(), exploding[i]['img'])
   }
 }
-setTimeout(display,2000);
-
+setTimeout(display,3000);
 
 
 
--- main_project/side_bar.js
+++ main_project/side_bar.js
@@ -1,16 +1,33 @@
+var menu_button_var = document.getElementById("menu_button");
+var comment_button_var = document.getElementById("comment_button");
+var back_button_var = document.getElementById("back_button");
+var nav_bar_menu = document.getElementById("nav-bar-menu");
 function openNav(sidebar, tab) {
   var style = document.getElementById(sidebar).style;
   style.width = "275px";
   style.shadow = "10px 10px";
   style.background="black";
   style.opacity ="0.8";
+  
+  back_button_var.style.position = "fixed";
+  back_button_var.style.left = "0px";
+  
+  menu_button_var.style.position = "fixed";
+  menu_button_var.style.left = "70px";
+  
+  comment_button_var.style.position = 'fixed';
+  comment_button_var.style.left = '140px';
+
+  nav_bar_menu.style.position = "fixed";
+  nav_bar_menu.style.top = "170px";
+
   if (tab == 'menu_tab'){
     open_tab('menu_link');
     close_tab('myForm');
   }
   else if(tab == 'comment_tab'){
     close_tab('menu_link');
-    open_tab('myForm') = 'block';
+    open_tab('myForm');
   }
   else{
     close_tab('myForm');
@@ -20,6 +37,12 @@ function openNav(sidebar, tab) {
 }
 
 function closeNav(sidebar) {
+  menu_button_var.style.position = 'relative';
+  menu_button_var.style.left = '0px';
+  comment_button_var.style.position = 'relative';
+  comment_button_var.style.left = '0px';
+  back_button_var.style.position = "relative";
+  back_button_var.style.left = "0px";
   var style = document.getElementById(sidebar).style;
   style.width = "7%";
   style.background="transparent";
@@ -33,10 +56,3 @@ function open_tab(tab){
 function close_tab(tab){
   document.getElementById(tab).style.display = 'none';
 }
-function remove_last_child(tab){
-  var menu = document.getElementById(tab);
-  menu.removeChild(menu.childNodes[4]);
-  menu.removeChild(menu.childNodes[3]);
-  menu.style.width = "7%";
-}
-
