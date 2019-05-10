d9c4357b8844ed90ecaa957ffd65e8abb2aa5b37
john-mai-2605 <50077665+john-mai-2605@users.noreply.github.com>
2019-05-11 02:22:24 +0900

    Edition page
    1/ Change the styling
    2/ Generalization (read data from Firebase database)

--- edition.js
+++ edition.js
@@ -1,4 +1,6 @@
-var check=0;
+var slideIndex
+var game_name = "Exploding Kitten";
+document.getElementById("game").innerHTML = game_name.toUpperCase();
 function openNav() {
   document.getElementById("mySidebar").style.width = "300px";
   document.getElementById("mySidebar").style.shadow = "10px 10px";
@@ -21,8 +23,6 @@ function closeNav() {
     a[i].style.color = "transparent";
   }
 }
-var slideIndex = 1;
-showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
@@ -35,10 +35,10 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
+  console.log('abc');
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("button");
-  var captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -49,14 +49,67 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-  captionText.innerHTML = 'Oneline description';
 }
-function choose(ref,status){
-  document.getElementById('collapsible'+ref).checked=status;
+function load_pic(i,name,len,des){
+  var url_pic=pr_loadEditionImage(name);
+  console.log(url_pic);
+  var slide = document.createElement("div");
+  slide.className="mySlides";
+  var num = document.createElement("div");
+  num.className="numbertext";
+  num.innerHTML = (i+1)+"/"+len;
+  var pic = document.createElement("img");
+  pic.src = url_pic;
+  pic.style.width="100%";
+  pic.className="pic";
+  var overlay = document.createElement('div');
+  overlay.className="overlay";
+  var button = document.createElement("button");
+  button.className="button";
+  button.id="but"
+  button.innerHTML='Read Tutorial';
+  var box = document.createElement('div');
+  box.className="caption-container";
+  var text = document.createElement('p');
+  text.id="caption";
+  text.innerHTML=des
+  overlay.appendChild(button);
+  box.appendChild(text);
+  slide.appendChild(num);
+  slide.appendChild(pic);
+  slide.appendChild(box);
+  slide.appendChild(overlay);
+  var left=document.getElementById('left');
+  document.getElementById("slide-container").insertBefore(slide,left);
+
+  }
+function load_button(i,name){
+  var col = document.createElement('div');
+  col.className="columnT";
+  var btn = document.createElement('button');
+  btn.className="button"; 
+  btn.style.width="100%";
+  btn.addEventListener("mouseover",function(){currentSlide(i+1)});
+  btn.innerHTML=name;
+  col.appendChild(btn);
+  document.getElementById("rowT").appendChild(col);
 }
-function total(){
-  check+=1;
-  for(i=1;i<8;i++){
-    choose(i,(check%2==1))
+
+function load_all_pic(name){
+  var edition_list = pr_loadGameEditionsList(name);
+  var len = edition_list.length
+  for(i=len-1;i>=0;i--){
+    desc = pr_loadEditionSectionText(edition_list[i],'briefing');
+    load_pic(i,edition_list[i],len,desc);
+    load_button(i,edition_list[i].replace(name,""));
+  des = pr_loadEditionSectionText(edition_list[0],'briefing');
+  slideIndex=1;
+  showSlides(slideIndex);
   }
 }
+
+setTimeout(function(){
+  load_all_pic(game_name);
+  document.getElementById("left").addEventListener("click",function(){plusSlides(-1)});
+  document.getElementById("right").addEventListener("click",function(){plusSlides(1)});
+}, 2000);
